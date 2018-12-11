573a5053419d9705154d21bb9da611fac3b142fc
pearlmiumiu <ll523@scarletmail.rutgers.edu>
2018-12-11 14:30:48 -0500

    base group html

---
+++ simplesocial/groups/views.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+# Create your views here.
+from django.contrib.auth.mixins import (LoginRequiredMixin, PermissionRequiredMixin)
+
+from django.core.urlresolvers import reverse
+
+from django.views import generic
+
+
+from groups.models import Group, GroupMember
+
+class CreateGroup(LoginRequiredMixin, generic.CreateView):
+	fields=('name', 'description')
+	model=Group
+
+
+class SingleGroup(generic.DetailView):
+	model=Group
+
+
+class ListGroups(generic.ListView):
+	model=Group
+
+
+
+
+
